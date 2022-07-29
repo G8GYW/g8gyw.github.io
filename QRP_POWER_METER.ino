4692f3b09c485fbdccf1bbf824d0f75803fcde68
G8GYW <66752188+G8GYW@users.noreply.github.com>
2022-07-29 10:20:39 +0100

    Update QRP_POWER_METER.ino

--- QRP_POWER_METER.ino
+++ QRP_POWER_METER.ino
@@ -12,6 +12,9 @@
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
 
+// Use of the MegunoLINK filter library is licensed by the copyright owners under the terms of the
+// GNU Lesser General Public License: https://github.com/Megunolink/MLP/blob/master/LICENSE.
+
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
